d4d202f40391f2615e2d3c1c6d62ec916bd28617
dustyzietz <dustyzietz@gmail.com>
2025-05-28 09:55:18 -1000

    fix broken link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -101,7 +101,7 @@ const config: Config = {
           items: [
             {
               label: 'Docs',
-              to: '/docs/intro',
+              to: '/docs',
             },
           ],
         },
